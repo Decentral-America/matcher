c7f1d4ebb2d48ae517d6d2703e7e1d5dea1384b1
bnagtega <brn@softwareag.com>
2021-05-30 19:12:06 +0200

    change packages from waves into dencentralchain

--- project/WavesNodeArtifactsPlugin.scala
+++ project/WavesNodeArtifactsPlugin.scala
@@ -89,8 +89,8 @@ object WavesNodeArtifactsPlugin extends AutoPlugin {
         // Additional steps
         // Unpacking waves-grpc-server extension JARs to have all required dependencies, including grpc ones
         val grpcServerArchive = targetDir.listFiles(new FileFilter {
-          override def accept(pathname: File): Boolean = pathname.name.matches("waves-grpc-server.+\\.tgz")
-        }).headOption.getOrElse(throw new RuntimeException("Can't find a waves-grpc-server archive"))
+          override def accept(pathname: File): Boolean = pathname.name.matches("decentralchain-grpc-server.+\\.tgz")
+        }).headOption.getOrElse(throw new RuntimeException("Can't find a decentralchain-grpc-server archive"))
 
         MatcherIOUtils.decompressTgz(grpcServerArchive, targetDir)
         val grpcServerDir = targetDir / grpcServerArchive.name.replace(".tgz", "")
